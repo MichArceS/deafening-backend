1fbc493cd5702c7b7b57f88ea674b4a3872aefcd
micxarce <micxarce@espol.edu.ec>
2022-06-02 17:35:22 -0500

    production configuration

--- config/config.js
+++ config/config.js
@@ -8,18 +8,14 @@ module.exports = {
     "port":"5432",
     "timezone": '-05:00'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
